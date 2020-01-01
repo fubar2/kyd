1b9543d760facabb3414f020d48e68f2b871db00
fubar2 <ross.lazarus@gmail.com>
2020-01-01 22:44:59 +1100

    Format code - something went wierd with layout.

--- python/dhcp-unknown.py
+++ python/dhcp-unknown.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import sys
 import requests
 import time
@@ -6,110 +8,130 @@ import argparse
 from collections import defaultdict
 
 
-def queryFingerbank(file_dic,key,proxy):
-	"""query Fingerbank API for the DHCP FPs provided in file_dic with the API key"""
+def queryFingerbank(file_dic, key, proxy):
+    """query Fingerbank API for the DHCP FPs provided in file_dic with the API key"""
 
-	headers = {'Content-type': 'application/json'}
-	url = 'https://api.fingerbank.org/api/v2/combinations/interrogate?key='+key
+    headers = {'Content-type': 'application/json'}
+    url = \
+        'https://api.fingerbank.org/api/v2/combinations/interrogate?key=' \
+        + key
 
-	resp_dic = defaultdict(list)
-	f = open("dhcp-db-FBQ", "w")
+    resp_dic = defaultdict(list)
+    f = open('dhcp-db-FBQ', 'w')
 
-	if "https" in proxy:
-		proxies={"https" : proxy}
-	else:
-		proxies={"http" : proxy}
+    if 'https' in proxy:
+        proxies = {'https': proxy}
+    else:
+        proxies = {'http': proxy}
 
-	for hash, fp in file_dic.items():
-		data = '{"dhcp_fingerprint":"' + fp +'"}'
-		resp_dic[hash].append(fp)
-		try:
-			# Try to get the data and json.load it 3 times, then give up
-			tries = 3
-			while tries >= 0:
-				try:
-					tries -= 1
-					if proxy == "not_set":
-						response = requests.post(url, headers=headers, data=data)
-					else:
-						response = requests.post(url, headers=headers, data=data, proxies=proxies)
-					json_response = json.loads(response.text)
+    for (hash, fp) in file_dic.items():
+        data = '{"dhcp_fingerprint":"' + fp + '"}'
+        resp_dic[hash].append(fp)
+        try:
 
-					if "device_name" not in json_response:
-						resp_dic[hash].append("Unknown in FB")
-						resp_dic[hash].append(0)
+            # Try to get the data and json.load it 3 times, then give up
 
-					else:
-						resp_dic[hash].append(json_response["device_name"])
-						resp_dic[hash].append(json_response["score"])
+            tries = 3
+            while tries >= 0:
+                try:
+                    tries -= 1
+                    if proxy == 'not_set':
+                        response = requests.post(url, headers=headers,
+                                data=data)
+                    else:
+                        response = requests.post(url, headers=headers,
+                                data=data, proxies=proxies)
+                    json_response = json.loads(response.text)
 
-					print('\n'+hash)
-					f.write(hash +'\t')
+                    if 'device_name' not in json_response:
+                        resp_dic[hash].append('Unknown in FB')
+                        resp_dic[hash].append(0)
+                    else:
 
-					for x in [0,1,2]:
-						s = str(resp_dic[hash][x]).strip('[]')
-						print('\t'+ s)
-						if isinstance(s,bytes):
-							f.write(s.encode("utf-8"))
-						else:
-							f.write(s)
-						f.write('\t')
-					f.write('\n')
-					break
+                        resp_dic[hash].append(json_response['device_name'
+                                ])
+                        resp_dic[hash].append(json_response['score'])
 
-				except:
-					print("Exception occured retrying")
-					if tries == 0:
-						# If we keep failing, raise the exception for the outer exception
-						# handling to deal with
-						raise
-					else:
-						# Wait a few seconds before retrying and hope the problem goes away
-						time.sleep(3)
-						tries -= 1
-						continue
+                    print '\n' + hash
+                    f.write(hash + '\t')
 
-		except:
-			print ("Oops! an exception have occured", sys.exc_info()[0])
-			raise
-		#finally:
-	f.close()
+                    for x in [0, 1, 2]:
+                        s = str(resp_dic[hash][x]).strip('[]')
+                        print '\t' + s
+                        if isinstance(s, bytes):
+                            f.write(s.encode('utf-8'))
+                        else:
+                            f.write(s)
+                        f.write('\t')
+                    f.write('\n')
+                    break
+                except:
+
+                    print 'Exception occured retrying'
+                    if tries == 0:
+
+                        # If we keep failing, raise the exception for the outer exception
+                        # handling to deal with
+
+                        raise
+                    else:
+
+                        # Wait a few seconds before retrying and hope the problem goes away
+
+                        time.sleep(3)
+                        tries -= 1
+                        continue
+        except:
+
+            print ('Oops! an exception have occured', sys.exc_info()[0])
+            raise
+
+        # finally:
+
+    f.close()
+
 
 def main():
-	"""Intake arguments from the user (file & API key) and output the dhcp-db-extend and dhcp-db-FBQ of the DHCP FP in FingerBank."""
+    """Intake arguments from the user (file & API key) and output the dhcp-db-extend and dhcp-db-FBQ of the DHCP FP in FingerBank."""
 
-	desc = "A python script for quering the Fingerbank API for unknown DHCP Fingerprints"
-	parser = argparse.ArgumentParser(description=(desc))
+    desc = \
+        'A python script for quering the Fingerbank API for unknown DHCP Fingerprints'
+    parser = argparse.ArgumentParser(description=desc)
 
-	help_text = "The api key for the FingerBank access"
-	parser.add_argument("-k", "--api_key", required=True, type=str, help=help_text)
+    help_text = 'The api key for the FingerBank access'
+    parser.add_argument('-k', '--api_key', required=True, type=str,
+                        help=help_text)
 
-	help_text = "File containing unknown DHCP Hashes and DHCP FP"
-	parser.add_argument("-f", "--file_unknown_hashes", required=True, type=str, help=help_text)
+    help_text = 'File containing unknown DHCP Hashes and DHCP FP'
+    parser.add_argument('-f', '--file_unknown_hashes', required=True,
+                        type=str, help=help_text)
 
-	help_text = "Proxy support for the isolated servers, give the proxy url as arg ex: http://ip:port"
-	parser.add_argument("-p", "--proxy", required=False, type=str, help=help_text)
+    help_text = \
+        'Proxy support for the isolated servers, give the proxy url as arg ex: http://ip:port'
+    parser.add_argument('-p', '--proxy', required=False, type=str,
+                        help=help_text)
 
-	args = parser.parse_args()
-	api_key = args.api_key
+    args = parser.parse_args()
+    api_key = args.api_key
 
-	file_dic = {}
+    file_dic = {}
 
-	with open(args.file_unknown_hashes) as f:
-		for line in f:
-			print(line)
-			(key, val) = line.split('\t')
-			key = key.strip()
-			val = val.strip()
-			file_dic[key] = val
-	f.close()
+    with open(args.file_unknown_hashes) as f:
+        for line in f:
+            print line
+            (key, val) = line.split('\t')
+            key = key.strip()
+            val = val.strip()
+            file_dic[key] = val
+    f.close()
 
-	if args.proxy:
-		proxy = args.proxy
-	else:
-		proxy = "not_set"
+    if args.proxy:
+        proxy = args.proxy
+    else:
+        proxy = 'not_set'
 
-	queryFingerbank(file_dic,api_key,proxy)
+    queryFingerbank(file_dic, api_key, proxy)
 
-if __name__ == "__main__":
-	main()
+
+if __name__ == '__main__':
+    main()
--- python/kyd.py
+++ python/kyd.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import argparse
 import dpkt
 import json
@@ -8,114 +10,143 @@ from hashlib import md5
 DHCP_PORT = 67
 BOOT_REQ = 1
 
+
 def convert_ip(value):
-	"""Convert an IP address from binary to text.
-	:param value: Raw binary data to convert
-	:type value: str
-	:returns: str
-	"""
-	try:
-		return socket.inet_ntop(socket.AF_INET, value)
-	except ValueError:
-		return socket.inet_ntop(socket.AF_INET6, value)
+    """Convert an IP address from binary to text.
+....:param value: Raw binary data to convert
+....:type value: str
+....:returns: str
+...."""
+
+    try:
+        return socket.inet_ntop(socket.AF_INET, value)
+    except ValueError:
+        return socket.inet_ntop(socket.AF_INET6, value)
+
 
 def process_pcap(pcap):
-	"""Process packets within the PCAP.
-	:param pcap: Opened PCAP file to be processed
-	:type pcap: dpkt.pcap.Reader
-	:returns: list of parsed records
-	"""
-	results = list()
+    """Process packets within the PCAP.
+....:param pcap: Opened PCAP file to be processed
+....:type pcap: dpkt.pcap.Reader
+....:returns: list of parsed records
+...."""
 
-	# Iterate through each packet in the pcap
-	for ts, pkt in pcap:
-		try:
-			eth = dpkt.ethernet.Ethernet(pkt)
-		except Exception:
-			continue
+    results = list()
 
-		if not isinstance(eth.data, dpkt.ip.IP):
-			# Only want IP packets
-			continue
+    # Iterate through each packet in the pcap
 
-		ip = eth.data
-		if ip.p != dpkt.ip.IP_PROTO_UDP:
-			# Only want UDP packets
-			continue
+    for (ts, pkt) in pcap:
+        try:
+            eth = dpkt.ethernet.Ethernet(pkt)
+        except Exception:
+            continue
 
-		udp = ip.data
-		try:
-			if not (udp.sport == DHCP_PORT or udp.dport == DHCP_PORT):
-			# Looking for the DHCP Packets
-				continue
-		except:
-			pass
-		try:
-			if len(udp.data) <= 0:
-				continue
-		except:
-			continue
-		bootstrap = bytearray(udp.data)
-		if bootstrap[0] != BOOT_REQ:
-			# only look for client to server DHCP messages (DHCP Reqs.)
-			continue
+        if not isinstance(eth.data, dpkt.ip.IP):
 
-		dhcp = dpkt.dhcp.DHCP(udp.data)
+            # Only want IP packets
 
-		int_vals = list()
-		# Iterate through the Options list of tuples
-		for opt in dhcp.opts:
-			if opt[0] == 55:
-				# Only look for the Param Req. List
-				data = bytearray(opt[1])
-				for i in range(0, len(data)):
-					element = data[i]
-					int_vals.append(element)
+            continue
 
-		dhcpfp = ",".join(str(x) for x in int_vals)
-		record = {"source_ip": convert_ip(ip.src),
-				  "destination_ip": convert_ip(ip.dst),
-				  "source_port": udp.sport,
-				  "destination_port": udp.dport,
-				  "DHCPFP": dhcpfp,
-				  "DHCPFP_hash": md5(dhcpfp.encode()).hexdigest(),
-				  "timestamp": ts}
-		results.append(record)
-	return results
+        ip = eth.data
+        if ip.p != dpkt.ip.IP_PROTO_UDP:
+
+            # Only want UDP packets
+
+            continue
+
+        udp = ip.data
+        try:
+            if not (udp.sport == DHCP_PORT or udp.dport == DHCP_PORT):
+
+            # Looking for the DHCP Packets
+
+                continue
+        except:
+            pass
+        try:
+            if len(udp.data) <= 0:
+                continue
+        except:
+            continue
+        bootstrap = bytearray(udp.data)
+        if bootstrap[0] != BOOT_REQ:
+
+            # only look for client to server DHCP messages (DHCP Reqs.)
+
+            continue
+
+        dhcp = dpkt.dhcp.DHCP(udp.data)
+
+        int_vals = list()
+
+        # Iterate through the Options list of tuples
+
+        for opt in dhcp.opts:
+            if opt[0] == 55:
+
+                # Only look for the Param Req. List
+
+                data = bytearray(opt[1])
+                for i in range(0, len(data)):
+                    element = data[i]
+                    int_vals.append(element)
+
+        dhcpfp = ','.join(str(x) for x in int_vals)
+        record = {
+            'source_ip': convert_ip(ip.src),
+            'destination_ip': convert_ip(ip.dst),
+            'source_port': udp.sport,
+            'destination_port': udp.dport,
+            'DHCPFP': dhcpfp,
+            'DHCPFP_hash': md5(dhcpfp.encode()).hexdigest(),
+            'timestamp': ts,
+            }
+        results.append(record)
+    return results
+
 
 def main():
-	"""Intake arguments from the user and print out kyd hash output."""
-	desc = "A python script for extracting DHCP fingerprints from PCAP files"
-	parser = argparse.ArgumentParser(description=(desc))
-	parser.add_argument("pcap", help="The pcap file to process")
+    """Intake arguments from the user and print out kyd hash output."""
+
+    desc = \
+        'A python script for extracting DHCP fingerprints from PCAP files'
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('pcap', help='The pcap file to process')
+
+    help_text = 'Print out as JSON records for downstream parsing'
+    parser.add_argument(
+        '-j',
+        '--json',
+        required=False,
+        action='store_true',
+        default=True,
+        help=help_text,
+        )
+    args = parser.parse_args()
+    filename = args.pcap
+
+    # Use an iterator to process each line of the file
+
+    output = None
+    with open(filename, 'rb') as fp:
+        try:
+            capture = dpkt.pcap.Reader(fp)
+        except ValueError, e:
+            raise Exception("File doesn't appear to be a PCAP: %s" % e)
+        output = process_pcap(capture)
+
+    if args.json:
+        output = json.dumps(output, indent=4, sort_keys=True)
+        print output
+    else:
+        for record in output:
+            tmp = '[{src}:{port}] kyd: {segment} --> {digest}'
+            tmp = tmp.format(src=record['source_ip'],
+                             port=record['source_port'],
+                             segment=record['DHCPFP'],
+                             digest=record['DHCPFP_hash'])
+            print tmp
 
 
-	help_text = "Print out as JSON records for downstream parsing"
-	parser.add_argument("-j", "--json", required=False, action="store_true",
-						default=True, help=help_text)
-	args = parser.parse_args()
-	filename = args.pcap
-	# Use an iterator to process each line of the file
-	output = None
-	with open(filename, 'rb') as fp:
-		try:
-			capture = dpkt.pcap.Reader(fp)
-		except ValueError as e:
-			raise Exception("File doesn't appear to be a PCAP: %s" % e)
-		output = process_pcap(capture)
-
-	if args.json:
-		output = json.dumps(output, indent=4, sort_keys=True)
-		print(output)
-	else:
-		for record in output:
-			tmp = '[{src}:{port}] kyd: {segment} --> {digest}'
-			tmp = tmp.format(src=record['source_ip'],
-							 port=record['source_port'],
-							 segment=record['DHCPFP'],
-							 digest=record['DHCPFP_hash'])
-			print(tmp)
-
-
-if __name__ == "__main__":
-	main()
+if __name__ == '__main__':
+    main()
