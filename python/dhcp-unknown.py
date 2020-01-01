368940a9201525a75d52fe24b17fe71ba17c82c2
fubar2 <ross.lazarus@gmail.com>
2020-01-02 09:59:48 +1100

    Modified dhcp-unknown so if no input tab delimited file is provided, it trawls through all log subdirectories looking for every dhcpfp log file, compressed or not, to locate unique hash/fingerprint combinations for lookup, bypassing the need to make a file at the expense of repeated lookups.

--- python/dhcp-unknown.py
+++ python/dhcp-unknown.py
@@ -1,137 +1,164 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 import sys
+import os
 import requests
 import time
 import json
 import argparse
 from collections import defaultdict
+import gzip
+
+
+def getallFPlogs(logpath='/usr/local/zeek/logs'):
+	""" find all subdirs of log and find all like dhcpfp.15:34:54-16:00:00.log.gz 
+		parse these into the required format. Return a dict keyed by hash of dhcp signatures
+	"""
+
+	dlist = os.listdir(logpath)
+	sigs = []
+	for dn in dlist:
+		dnpath = os.path.join(logpath, dn)
+		if os.path.isdir(dnpath):
+			flist = os.listdir(dnpath)
+			flist = [x for x in flist if x.startswith('dhcpfp')]
+			for fn in flist:
+				fnpath = os.path.join(dnpath, fn)
+				if fnpath.endswith('log.gz'):
+					dat = gzip.open(fnpath, 'rt').readlines()
+				else:
+					dat = open(fnpath, 'r').readlines()
+				dat = [x for x in dat if not x.startswith('#')]
+				dats = [x.split()[8:11] for x in dat]
+				sigs += dats
+	return dict(sigs)  # removes dupes
 
 
 def queryFingerbank(file_dic, key, proxy):
-    """query Fingerbank API for the DHCP FPs provided in file_dic with the API key"""
+	"""query Fingerbank API for the DHCP FPs provided in file_dic with the API key"""
 
-    headers = {'Content-type': 'application/json'}
-    url = \
-        'https://api.fingerbank.org/api/v2/combinations/interrogate?key=' \
-        + key
+	headers = {'Content-type': 'application/json'}
+	url = \
+		'https://api.fingerbank.org/api/v2/combinations/interrogate?key=' \
+		+ key
 
-    resp_dic = defaultdict(list)
-    f = open('dhcp-db-FBQ', 'w')
+	resp_dic = defaultdict(list)
+	f = open('dhcp-db-FBQ', 'w')
 
-    if 'https' in proxy:
-        proxies = {'https': proxy}
-    else:
-        proxies = {'http': proxy}
+	if 'https' in proxy:
+		proxies = {'https': proxy}
+	else:
+		proxies = {'http': proxy}
 
-    for (hash, fp) in file_dic.items():
-        data = '{"dhcp_fingerprint":"' + fp + '"}'
-        resp_dic[hash].append(fp)
-        try:
+	for (hash, fp) in file_dic.items():
+		data = '{"dhcp_fingerprint":"' + fp + '"}'
+		resp_dic[hash].append(fp)
+		try:
 
-            # Try to get the data and json.load it 3 times, then give up
+			# Try to get the data and json.load it 3 times, then give up
 
-            tries = 3
-            while tries >= 0:
-                try:
-                    tries -= 1
-                    if proxy == 'not_set':
-                        response = requests.post(url, headers=headers,
-                                data=data)
-                    else:
-                        response = requests.post(url, headers=headers,
-                                data=data, proxies=proxies)
-                    json_response = json.loads(response.text)
+			tries = 3
+			while tries >= 0:
+				try:
+					tries -= 1
+					if proxy == 'not_set':
+						response = requests.post(url, headers=headers,
+								data=data)
+					else:
+						response = requests.post(url, headers=headers,
+								data=data, proxies=proxies)
+					json_response = json.loads(response.text)
 
-                    if 'device_name' not in json_response:
-                        resp_dic[hash].append('Unknown in FB')
-                        resp_dic[hash].append(0)
-                    else:
+					if 'device_name' not in json_response:
+						resp_dic[hash].append('Unknown in FB')
+						resp_dic[hash].append(0)
+					else:
 
-                        resp_dic[hash].append(json_response['device_name'
-                                ])
-                        resp_dic[hash].append(json_response['score'])
+						resp_dic[hash].append(json_response['device_name'
+								])
+						resp_dic[hash].append(json_response['score'])
 
-                    print '\n' + hash
-                    f.write(hash + '\t')
+					print('\n' + hash)
+					f.write(hash + '\t')
 
-                    for x in [0, 1, 2]:
-                        s = str(resp_dic[hash][x]).strip('[]')
-                        print '\t' + s
-                        if isinstance(s, bytes):
-                            f.write(s.encode('utf-8'))
-                        else:
-                            f.write(s)
-                        f.write('\t')
-                    f.write('\n')
-                    break
-                except:
+					for x in [0, 1, 2]:
+						s = str(resp_dic[hash][x]).strip('[]')
+						print('\t' + s)
+						if isinstance(s, bytes):
+							f.write(s.encode('utf-8'))
+						else:
+							f.write(s)
+						f.write('\t')
+					f.write('\n')
+					break
+				except:
 
-                    print 'Exception occured retrying'
-                    if tries == 0:
+					print('Exception encountered. Retrying')
+					if tries == 0:
 
-                        # If we keep failing, raise the exception for the outer exception
-                        # handling to deal with
+						# If we keep failing, raise the exception for the outer exception
+						# handling to deal with
 
-                        raise
-                    else:
+						raise
+					else:
 
-                        # Wait a few seconds before retrying and hope the problem goes away
+						# Wait a few seconds before retrying and hope the problem goes away
 
-                        time.sleep(3)
-                        tries -= 1
-                        continue
-        except:
+						time.sleep(3)
+						tries -= 1
+						continue
+		except:
 
-            print ('Oops! an exception have occured', sys.exc_info()[0])
-            raise
+			print ('Oops! an exception has occurred', sys.exc_info()[0])
+			raise
 
-        # finally:
+		# finally:
 
-    f.close()
+	f.close()
 
 
 def main():
-    """Intake arguments from the user (file & API key) and output the dhcp-db-extend and dhcp-db-FBQ of the DHCP FP in FingerBank."""
+	"""Intake arguments from the user (file & API key) and output the dhcp-db-extend and dhcp-db-FBQ of the DHCP FP in FingerBank."""
 
-    desc = \
-        'A python script for quering the Fingerbank API for unknown DHCP Fingerprints'
-    parser = argparse.ArgumentParser(description=desc)
+	desc = \
+		'Query the Fingerbank API for DHCP Fingerprints from either a tab delimited file (dhcphash and dhcpfp as columns) or comprehensively from all available zeek logs'
+	parser = argparse.ArgumentParser(description=desc)
 
-    help_text = 'The api key for the FingerBank access'
-    parser.add_argument('-k', '--api_key', required=True, type=str,
-                        help=help_text)
+	help_text = 'Api key for the FingerBank access'
+	parser.add_argument('-k', '--api_key', required=True, type=str,
+						help=help_text)
 
-    help_text = 'File containing unknown DHCP Hashes and DHCP FP'
-    parser.add_argument('-f', '--file_unknown_hashes', required=True,
-                        type=str, help=help_text)
+	help_text = 'Optional Ffile containing unknown DHCP Hashes and DHCP FP'
+	parser.add_argument('-f', '--file_unknown_hashes', required=False,
+						type=str, help=help_text)
 
-    help_text = \
-        'Proxy support for the isolated servers, give the proxy url as arg ex: http://ip:port'
-    parser.add_argument('-p', '--proxy', required=False, type=str,
-                        help=help_text)
+	help_text = \
+		'Proxy support for the isolated servers, give the proxy url as arg ex: http://ip:port'
+	parser.add_argument('-p', '--proxy', required=False, type=str,
+						help=help_text)
 
-    args = parser.parse_args()
-    api_key = args.api_key
+	args = parser.parse_args()
+	api_key = args.api_key
+	if args.file_unknown_hashes:
+		fp_dic = {}
+		with open(args.file_unknown_hashes) as f:
+			for line in f:
+				print(line)
+				(key, val) = line.split('\t')
+				key = key.strip()
+				val = val.strip()
+				fp_dic[key] = val
+		f.close()
+	else:
+		fp_dic = getallFPlogs()
+	if args.proxy:
+		proxy = args.proxy
+	else:
+		proxy = 'not_set'
 
-    file_dic = {}
-
-    with open(args.file_unknown_hashes) as f:
-        for line in f:
-            print line
-            (key, val) = line.split('\t')
-            key = key.strip()
-            val = val.strip()
-            file_dic[key] = val
-    f.close()
-
-    if args.proxy:
-        proxy = args.proxy
-    else:
-        proxy = 'not_set'
-
-    queryFingerbank(file_dic, api_key, proxy)
+	queryFingerbank(fp_dic, api_key, proxy)
 
 
 if __name__ == '__main__':
-    main()
+	main()
