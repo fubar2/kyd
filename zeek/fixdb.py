e9d5ffff1e129eb725a4dc57d7122eeb13ec5be8
fubar2 <ross.lazarus@gmail.com>
2020-01-02 14:30:39 +1100

    Madness prevails - wrong filename. Hilarity ensues.

--- zeek/fixdb.py
+++ zeek/fixdb.py
@@ -3,10 +3,11 @@
 foo = open('dhcp-db.txt','r').readlines()
 bar = [x.replace('\n','') for x in foo]
 zot = [x.split('\t') for x in bar]
-l = [len(x) for x in zot]
-foo = ['\t'.join(x) for x in zot]
-print('foo',foo[:10])
-f = open('dhcp-db.txt.fixed','w')
-f.write('\n'.join(foo))
-f.write('\n')
-print(l)
+l = [len(x) for x in zot if len(x) != 4]
+print('l = (should be header only)',l)
+if len(l) > 1:
+	foo = ['\t'.join(x) for x in zot]
+	print('foo',foo[:10])
+	f = open('dhcp-db.txt.fixed','w')
+	f.write('\n'.join(foo))
+	f.write('\n')
