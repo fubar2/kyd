810f28fa227386da823348fa8e7901cb059cf97b
fubar2 <ross.lazarus@gmail.com>
2020-01-02 14:17:41 +1100

    Discovered that the dhcp-db.txt file had a mixture of tab counts - a bunch of lines had a bogus tab before the newline and this may be causing the file to never be read properly

---
+++ zeek/fixdb.py
@@ -0,0 +1,12 @@
+# turns out the database was probably never read properly!
+# as some lines have a line ending after a tab!
+foo = open('dhcp-db.txt','r').readlines()
+bar = [x.replace('\n','') for x in foo]
+zot = [x.split('\t') for x in bar]
+l = [len(x) for x in zot]
+foo = ['\t'.join(x) for x in zot]
+print('foo',foo[:10])
+f = open('dhcp-db.txt.fixed','w')
+f.write('\n'.join(foo))
+f.write('\n')
+print(l)
